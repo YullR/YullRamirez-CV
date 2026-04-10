17c0a288a3f4e4117485235e338e468b997d9add
YullR <YullR@outlook.com>
2026-04-10 15:41:51 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -153,7 +153,7 @@ const App = () => {
     setIsTyping(true);
 
     try {
-      const apiKey = ""; 
+      const apiKey = import.meta.env.VITE_GEMINI_API_KEY; 
       const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${apiKey}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
